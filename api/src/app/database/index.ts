169a6c8a9d27ed6dc969e2dba46553970f837cd6
AntonioGally <antonio.gally@gmail.com>
2023-02-13 23:09:50 -0300

    removing envs and database config

--- api/src/app/database/index.ts
+++ api/src/app/database/index.ts
@@ -2,28 +2,14 @@ const { Client } = require('pg');
 
 const isLocal =
     process.env.LOCAL_DEVELOPMENT === "false" ? false : true || false;
-
-let client;
-if (isLocal) {
-    client = new Client({
-        host: 'localhost',
-        port: 5432,
-        user: 'root',
-        password: 'root',
-        database: 'magazinereader',
-    });
-} else {
-    client = new Client({
-        host: "ec2-3-217-251-77.compute-1.amazonaws.com",
-        port: 5432,
-        user: "qendnwqtwpkttj",
-        password: process.env.POSTGRES_SECRET,
-        database: "d90k3g0im7k88m",
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-}
+console.log(process.env.POSTGRES_HOST)
+const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: 'root',
+    password: 'root',
+    database: 'magazinereader',
+});
 
 
 client.connect(function (err) {
