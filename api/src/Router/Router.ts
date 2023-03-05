a9b7a831017cb52c0bada6adff27737a0bede78e
AntonioGally <antonio.gally@gmail.com>
2023-03-04 23:46:34 -0300

    feat (api): Adding user routes

--- api/src/Router/Router.ts
+++ api/src/Router/Router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthRoutes from "./Auth/Auth.routes";
 import EditionsRouter from "./Editions/Editions.routes";
 import MagazinesRouter from "./Magazines/Magazines.routes";
+import UserRouter from "./User/User.routes";
 //Controllers
 
 export const router = Router();
@@ -9,3 +10,4 @@ export const router = Router();
 new AuthRoutes(router).execute();
 new MagazinesRouter(router).execute();
 new EditionsRouter(router).execute();
+new UserRouter(router).execute();
