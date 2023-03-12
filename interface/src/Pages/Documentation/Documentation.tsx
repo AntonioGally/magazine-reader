1ee870b137f82c2fb4d1e8c823c2f9a81d25cdb4
AntonioGally <antonio.gally@gmail.com>
2023-03-12 13:57:06 -0300

    feat (interface): Adding editions documentation

--- interface/src/Pages/Documentation/Documentation.tsx
+++ interface/src/Pages/Documentation/Documentation.tsx
@@ -3,6 +3,7 @@ import { Divider } from "antd";
 import React from "react";
 //Css
 import style from "./documentation.module.css";
+import Editions from "./Sections/Editions";
 //Sections
 import Magazine from "./Sections/Magazine";
 import NewEditions from "./Sections/NewEditions";
@@ -10,12 +11,19 @@ import NewEditions from "./Sections/NewEditions";
 const Documentation: React.FC = () => {
     return (
         <div className={style["wrapper"]}>
+            <section id="about">
+        
+            </section>
             <section id="magazine">
                 <Magazine />
             </section>
             <Divider />
             <section id="new-editions">
                 <NewEditions />
+            </section>
+            <Divider />
+            <section id="editions">
+                <Editions />
             </section>
         </div>
     )
