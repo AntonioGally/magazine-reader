0a1b3941f10f7c5256418a398fdac6e11674a7fc
AntonioGally <antonio.gally@gmail.com>
2023-03-12 11:55:00 -0300

    feat (interface): Adding magazine documentation

--- interface/src/Pages/Documentation/Documentation.tsx
+++ interface/src/Pages/Documentation/Documentation.tsx
@@ -2,11 +2,18 @@
 import React from "react";
 //Css
 import style from "./documentation.module.css";
+//Sections
+import Magazine from "./Sections/Magazine";
 
 const Documentation: React.FC = () => {
     return (
         <div className={style["wrapper"]}>
-            <span>Em desenvolvimento</span>
+            <section id="magazine">
+                <Magazine />
+            </section>
+            <section>
+                
+            </section>
         </div>
     )
 }
